51d6a3e58d8a3ad2a78d3961682511f28f0dccbe
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2020-10-19 14:42:10 +0200

    update

--- ui.R
+++ ui.R
@@ -1,33 +1,39 @@
-ui<-navbarPage("TITOLO",
-    theme = shinytheme("cerulean"),
+ui<-fluidPage("Cinetica d'inattivazione virale",
+    theme = shinytheme("cerulean"), 
+    
+    sidebarLayout(
+                   sidebarPanel("Input data"),
+                   mainPanel("Risultati")
+                 )
+    
            
-    tabPanel("TAB1",
-             sidebarLayout(
-               sidebarPanel(),
-               mainPanel()
-             )
-    ),
-    
-    tabPanel("TAB2",
-             sidebarLayout(
-               sidebarPanel(),
-               mainPanel()
-             )
-    ), 
-                   
-    tabPanel("TAB2",
-             sidebarLayout(
-               sidebarPanel(),
-               mainPanel()
-             )
-    ), 
-    
-    tabPanel("....",
-             sidebarLayout(
-                 sidebarPanel(),
-                 mainPanel()
-             )
-    )     
-)         
-             
+    # tabPanel("TAB1",
+    #          sidebarLayout(
+    #            sidebarPanel(),
+    #            mainPanel()
+    #          )
+    # ),
+    # 
+    # tabPanel("TAB2",
+    #          sidebarLayout(
+    #            sidebarPanel(),
+    #            mainPanel()
+    #          )
+    # ), 
+    #                
+    # tabPanel("TAB2",
+    #          sidebarLayout(
+    #            sidebarPanel(),
+    #            mainPanel()
+    #          )
+    # ), 
+    # 
+    # tabPanel("....",
+    #          sidebarLayout(
+    #              sidebarPanel(),
+    #              mainPanel()
+    #          )
+    # )     
+  )        
+       
 
