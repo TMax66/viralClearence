49a797979960da6438db889c8712cd6d5bee3be3
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2020-10-20 13:42:40 +0200

    update

--- ui.R
+++ ui.R
@@ -1,51 +1,7 @@
-# ui<-fluidPage("Cinetica d'inattivazione virale",
-#     theme = shinytheme("cerulean"), 
-#     
-#     sidebarLayout(
-#                    sidebarPanel(sidebarPanel(
-#                      textInput('vec1', 'Enter a vector (comma delimited)', "")
-#                    )),
-#                    mainPanel("Risultati", 
-#                              tableOutput("t"))
-#                  )
-#     
-           
-    # tabPanel("TAB1",
-    #          sidebarLayout(
-    #            sidebarPanel(),
-    #            mainPanel()
-    #          )
-    # ),
-    # 
-    # tabPanel("TAB2",
-    #          sidebarLayout(
-    #            sidebarPanel(),
-    #            mainPanel()
-    #          )
-    # ), 
-    #                
-    # tabPanel("TAB2",
-    #          sidebarLayout(
-    #            sidebarPanel(),
-    #            mainPanel()
-    #          )
-    # ), 
-    # 
-    # tabPanel("....",
-    #          sidebarLayout(
-    #              sidebarPanel(),
-    #              mainPanel()
-    #          )
-    # )     
- # )        
-       
-
-
-library("shiny")    
 shinyUI(
   
   pageWithSidebar(
-    theme = shinytheme("cerulean"),
+   
     headerPanel("Cinetica d'inattivazione virale")
     ,
     sidebarPanel(
